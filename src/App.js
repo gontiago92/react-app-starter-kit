9aada8d506b8663bd47aa8cceb83552c2c716873
Tiago Gonçalves <tiago.goncalves.faria@gmail.com>
2021-07-14 07:58:48 +0200

    Creation of Starter Kit
    Starter Kit for React projects. Lighter than the usual create-react-app command.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>React App Starter Kit</h1>
+            <p>An easier and faster way to begin your React projects.</p>
+        </div>
+    )
+}
+
+export default App
